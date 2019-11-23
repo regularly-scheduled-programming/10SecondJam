37642ed9c170e215ae85c4b45d2737f9587703a6
poboivin <poboivin@hotmail.com>
2019-11-23 14:16:26 -0500

    player and bounds

--- Assets/Script/Pier/LineofSight.cs
+++ Assets/Script/Pier/LineofSight.cs
@@ -6,11 +6,22 @@ public class LineofSight : MonoBehaviour
 {
     public float x = 5;
     public float y = 5;
+
+    public Vector3 topLeft;
+    public Vector3 topRight;
+    public Vector3 bottomLeft;
+    public Vector3 bottomRight;
+
     public Obstacle[] obstacles;
     // Start is called before the first frame update
     void Start()
     {
         obstacles = FindObjectsOfType<Obstacle>();
+
+        topRight = new Vector3(x / 2, y / 2, 1);
+        topLeft = new Vector3(-x / 2, y / 2, 1);
+        bottomRight = new Vector3(x / 2, -y / 2, 1);
+        bottomLeft = new Vector3(-x / 2, -y / 2, 1);
     }
 
     private void OnDrawGizmos()
@@ -18,10 +29,10 @@ public class LineofSight : MonoBehaviour
         Gizmos.color = Color.green;
         Gizmos.DrawWireCube(Vector3.zero, new Vector3(x, y, 1));
 
-        Gizmos.DrawLine(this.transform.position, new Vector3(x/2, y/2, 1));
-        Gizmos.DrawLine(this.transform.position, new Vector3(-x/2, y/2, 1));
-        Gizmos.DrawLine(this.transform.position, new Vector3(x/2, -y/2, 1));
-        Gizmos.DrawLine(this.transform.position, new Vector3(-x/2, -y/2, 1));
+        Gizmos.DrawLine(this.transform.position, topRight);
+        Gizmos.DrawLine(this.transform.position, topLeft);
+        Gizmos.DrawLine(this.transform.position, bottomRight);
+        Gizmos.DrawLine(this.transform.position, bottomLeft);
         foreach (var b in obstacles)
         {
            
