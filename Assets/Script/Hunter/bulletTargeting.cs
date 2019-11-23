5b49444bbe44e21073b6d6f8e329724ce35aee11
Hunter Fraser <hunterfraser.gamedev@gmail.com>
2019-11-23 13:38:55 -0500

    prefabfolder

---
+++ Assets/Script/Hunter/bulletTargeting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletTargeting : MonoBehaviour
+{
+    //Vector2 ShootingLocation;
+    public GameObject PlaceHolder;
+    [SerializeField]
+    GameObject bulletSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+
+
+    public void SpawnBullet(Vector3 targetLocation)
+    {
+        Vector2 rawDirection = targetLocation - transform.position;
+
+        Vector2 direcetion = rawDirection / rawDirection.magnitude;
+
+       // Instantiate()
+
+
+
+        //GameObject Bullet= Instantiate()
+    }
+}
