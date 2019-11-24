5094427da3acd34da3ee7b3758042d464e832b14
Ian McCamant <ian.mccamant@georgebrown.ca>
2019-11-23 21:59:47 -0500

    extended UI functionality

---
+++ Assets/Script/Ian/BaseButton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseButton: MonoBehaviour
+{
+    public bool WasClicked;
+    public Transform SubMenu;
+    public GameObject[] Cards;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void MakeActiveButton()
+    {
+        if (!WasClicked)
+        {
+            Debug.Log("I have been clicked");
+            WasClicked = true;
+            OpenSubMenu();
+            foreach (var Card in Cards)
+            {
+                Color CurrentColor = Card.GetComponent<Image>().color;
+                Card.GetComponent<Button>().interactable = false;
+            } 
+        }
+        else {
+            Debug.Log("I have been unclicked");
+            WasClicked = false;
+            CloseSubMenu();
+            foreach (var Card in Cards)
+            {
+                Color CurrentColor = Card.GetComponent<Image>().color;
+                Card.GetComponent<Button>().interactable = true;
+            }
+        }
+    }
+    public void OpenSubMenu() {
+        if (SubMenu)
+        {
+            SubMenu.gameObject.SetActive(true);
+        }
+    }
+    public void CloseSubMenu()
+    {
+        if (SubMenu)
+        {
+            SubMenu.gameObject.SetActive(false);
+        }  
+    }
+}
---
+++ Assets/Script/Ian/TimelineBehavior.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimelineBehavior : MonoBehaviour
+{
+    public float scrollSpeed = 1;
+    public float currentXPosition = 0;
+    public float minXPostion;
+    public ActionWidget[] MyActions;
+    private RectTransform rectTransform;
+    private ContentSizeFitter contentSizeFitter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        contentSizeFitter = GetComponent<ContentSizeFitter>();
+        contentSizeFitter.enabled = false;
+        Invoke("FixContentSizeFitter", Time.deltaTime);
+        UpdateActionList();
+    }
+    [ContextMenu("UpdateActionList")]
+    public void UpdateActionList()
+    {
+        MyActions = GetComponentsInChildren<ActionWidget>();
+        minXPostion = 0;//reset max position
+        //loop for all action
+        foreach (var action in MyActions)
+        {
+            minXPostion -= action.WarmUpFrames * action.framemultiplier;
+            minXPostion -= action.ActiveFrames * action.framemultiplier;
+            minXPostion -= action.CooldownFrames * action.framemultiplier;
+        }
+
+        Time.timeScale = 1;
+
+    }
+    void FixContentSizeFitter()
+    {
+        contentSizeFitter.enabled = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(currentXPosition > minXPostion)
+        {
+            currentXPosition -= scrollSpeed * Time.deltaTime;
+            rectTransform.anchoredPosition3D = new Vector3(currentXPosition, rectTransform.anchoredPosition3D.y);
+
+        }
+        else
+        {
+            currentXPosition = minXPostion;
+            Time.timeScale = 0;
+        }
+    }
+}
